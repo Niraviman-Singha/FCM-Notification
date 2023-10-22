ca2bbd3f44a9dab53404d6bfbc081209b3eda3c3
Niraviman <niravimansingharoy71@gmail.com>
2023-10-22 12:19:42 +0600

    Firebase Cloud Messaging

--- app/src/main/java/com/example/fcm_notification/MainActivity.kt
+++ app/src/main/java/com/example/fcm_notification/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.fcm_notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fcm_notification.databinding.ActivityMainBinding
+import java.security.Key
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    var dataString: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        if (intent != null) {
+            if (intent.hasExtra("key1")) {
+                for (key in intent.extras!!.keySet()) {
+                    dataString = dataString + intent.extras!!.getString(key) + "\n"
+                }
+                binding.textView.text = dataString
+            }
+        }
     }
 }
